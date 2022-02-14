6899c8e5c7d1f585249e5964e25d2bd747d4f20a
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-02-14 23:50:35 +0530

    cmt0214

--- cypress/integration/testSuite03/ts04-checkBox.js
+++ cypress/integration/testSuite03/ts04-checkBox.js
@@ -11,15 +11,38 @@ describe('sample E2E tests', function() {
     it.only('test01:  Ecommerce page', ()=> {
         cy.visit('http://automationpractice.com/index.php',{failOnStatusCode: false})
         cy.get('.sf-with-ul').first().click()
+        
         //check the checkboxes and verify
         cy.get('.checkbox').check().parent().should('have.class','checked')
+        
         //uncheck the checkboxes and verify
         cy.get('.checkbox').uncheck().parent().should('not.have.class','checked')
+        
         cy.get('#layered_id_feature_5').check({force:true})
 
         cy.log('End of test')
     })
 
+    it.only('test02: Ecommerce page', () => {
+        cy.visit('https://www.snapdeal.com/')
+        cy.get('#inputValEnter').type('Laptop')
+        cy.contains('Search').click()
+        
+        //check the checkboxes and verify
+        cy.get('[data-displayname="Brand"] > .filter-content > .filter-inner > :nth-child(n) > input')
+            .check({force: true})
+        cy.get('[data-displayname="Brand"] > .filter-content > .filter-inner > :nth-child(n) > input')
+            .should('have.checked', 'checked')
+
+        //uncheck the checkboxes and verify
+        cy.get('[data-displayname="Brand"] > .filter-content > .filter-inner > :nth-child(n) > input')
+            .uncheck({force: true})
+        cy.get('[data-displayname="Brand"] > .filter-content > .filter-inner > :nth-child(n) > input')
+            .should('not.have.checked', 'checked')
+        
+        cy.log('End of test')
+        
+    })
     
 
 }) 
