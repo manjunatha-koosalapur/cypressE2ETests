186944fd60f0ea5f55fcda3dd65478aa5c221b3d
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-02-14 13:29:42 +0530

    cmt2

---
+++ cypress/integration/testSuite01/ts01-LEVYHealth.js
@@ -0,0 +1,79 @@
+/// <reference types="cypress" />
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+    });
+
+describe('LEVY Health tests', function() {
+
+    it('test01: LEVY home page', ()=> {
+        cy.visit('https://www.levy.health/en/',{failOnStatusCode: false})  //failOnStatusCode is to Handle Service Unavailable and Uncaught Exception in Cypress
+        cy.url().should('include','levy.health/en')
+        cy.title().should('contains','Levy Health')
+        cy.get('#menu-item-wpml-ls-15-de > a > .wpml-ls-native').click()
+        cy.url().should('include','levy.health')
+        cy.wait(4000)
+    })
+
+    it('test02: our science', ()=> {
+        cy.visit('https://www.levy.health/en/')
+        cy.get('#menu-main-menu-en-1 > .menu-item-564 > a').click()
+        cy.url().should('include','levy.health/en/our-science/')
+        cy.contains('LEVY Fertility Code').should('be.visible')
+        cy.contains('The algorithm creates your personal risk profile').click()
+        cy.contains('Individualized blood diagnostics').click()
+        cy.contains('You receive your results and discuss them with our reproductive physicians').should('be.visible')
+        cy.contains('We accompany you holistically').click()
+        cy.wait(4000)
+
+    })
+
+    it('test03: team', ()=> {
+        cy.visit('https://www.levy.health/en/')
+        cy.get('#menu-main-menu-en-1 > .menu-item-563 > a').click()
+        cy.url().should('include','levy.health/en/team/')
+        cy.contains('THE FOUNDERS').should('be.visible')
+        cy.contains('CAROLINE MITTERDORFER').should('be.visible')
+        cy.contains('SILVIA HECHER').should('be.visible')
+        cy.contains('THERESA VILSMAIER').should('be.visible')
+        cy.contains('SCOTT ENGLAND')
+        cy.wait(4000)
+
+    })
+
+    it('test04: blog', ()=> {
+        cy.visit('https://www.levy.health/en/')
+        cy.get('#menu-main-menu-en-1 > .menu-item-3391 > a').click()
+        cy.url().should('include','levy.health/en/blog/')
+        cy.contains('LEVY Blog').should('be.visible')
+        cy.wait(4000)
+        
+    })
+
+    it('test05: contact', ()=> {
+        cy.visit('https://www.levy.health/en/')
+        cy.get('#menu-main-menu-en-1 > .menu-item-565 > a').click()
+        cy.url().should('include','levy.health/en/contact/')
+        cy.contains('Contact Us').should('be.visible')
+        cy.contains('hello@levy.health').should('be.visible')
+        cy.wait(4000)
+        
+    })
+
+    it('test06: login', ()=> {
+        cy.visit('https://www.levy.health/en/')
+        cy.get('#menu-main-menu-en-1 > .menu-item-type-custom > a').click()
+        cy.url().should('include','your.levy.health/auth/register')
+        cy.get('.picker > :nth-child(2)').click()
+        cy.contains('Sign up to LEVY').should('be.visible')
+        cy.contains('Already a member? Log in').should('be.visible')
+        cy.contains('I agree to the Terms and Conditions and Data Privacy Policy').should('be.visible')
+        cy.contains('Sign up to LEVY').should('be.visible')
+        cy.wait(4000)
+        
+    })
+
+
+})  
---
+++ cypress/integration/testSuite01/ts02-viewPortLEVY.js
@@ -0,0 +1,63 @@
+/// <reference types="cypress" />
+// --* reference *--//
+// https://docs.cypress.io/api/commands/viewport
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+    });
+
+describe('view ports: size & orientation of the LEVY web pages', function() {
+    before( () =>{
+        console.log('running my tests')
+    })
+
+    beforeEach( ()=>{
+        cy.visit('https://www.levy.health/en/')
+    })
+
+    it('test01: run tests in macbook-16', ()=> {
+        cy.viewport('macbook-16')
+        cy.screenshot()
+        cy.wait(2000)
+    })
+
+    it('test02: run tests in ipad-2', ()=> {
+        cy.viewport('ipad-2')
+        cy.wait(2000)
+    })
+
+    it('test03: run tests in iphone-8', ()=> {
+        cy.viewport('iphone-8')
+        cy.wait(2000)
+    })
+
+    it('test04: run tests in 1024px x 768px', ()=> {
+        cy.viewport(1024,768)
+        cy.wait(2000)
+    })
+
+    it('test05: run tests in samsung-s10', ()=> {
+        cy.viewport('samsung-note9')
+        cy.wait(2000)
+        cy.get('.bar-img').click()
+        cy.get('#menu-item-563 > a').click()
+        cy.url().should('include','levy.health/en/team/')
+        cy.contains('THE FOUNDERS').should('be.visible')
+        cy.contains('CAROLINE MITTERDORFER').should('be.visible')
+        cy.contains('SILVIA HECHER').should('be.visible')
+        cy.contains('THERESA VILSMAIER').should('be.visible')
+        cy.contains('SCOTT ENGLAND')
+        cy.wait(4000)
+    })
+
+    it('test06: LEVY Health Social link - LinkedIn', ()=> {
+        cy.visit('https://www.levy.health/en/')
+        cy.get(':nth-child(3) > a > img').click()
+        cy.url().should('include','linkedin.com/company/')
+        
+        
+    })
+
+})
